5c4d4c585bf72216b7f4381b16ee272d4646e6cc
LEE13262201 <bilibidbinilibid@gmail.com>
2024-10-31 15:31:28 +0800

    Landing Page component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { VoicesComponent } from './components/voices/voices.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { AboutComponent } from './components/about/about.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
-    { path: '', component: VoicesComponent },
+    { path: '', component: LandingComponent },
     { path: 'voices', component: VoicesComponent },
     { path: 'feedback', component: FeedbackComponent },
     { path: 'about', component: AboutComponent },
