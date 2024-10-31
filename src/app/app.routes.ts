68f8f54e68db726d7886aca3ecb5d6f04900f085
LEE13262201 <bilibidbinilibid@gmail.com>
2024-10-31 11:32:02 +0800

    Some minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { VoicesComponent } from './components/voices/voices.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     { path: '', component: VoicesComponent },
     { path: 'voices', component: VoicesComponent },
     { path: 'feedback', component: FeedbackComponent },
+    { path: 'about', component: AboutComponent },
     { path: '**', redirectTo: '' }
 ];
